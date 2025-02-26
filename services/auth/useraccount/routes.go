a658673dd5b5be6b23a37b9713a3344ad3eac1cb
omkar <opwebdev01@gmail.com>
2025-02-26 16:39:02 +0530

    latest chnage

--- services/auth/useraccount/routes.go
+++ services/auth/useraccount/routes.go
@@ -79,7 +79,7 @@ func (h *Handler) registerUser(c router.Context) error {
 
 		return errutil.AlreadyExist("User")
 	} else if err != nil {
-		fmt.Println(">>>>>>>>>> err occured", err)
+		fmt.Println(">>>>>>>>>> err")
 		return err
 	}
 
@@ -92,7 +92,7 @@ func (h *Handler) login(c router.Context) error {
 	ctx, cancel := createContext()
 	defer cancel()
 
-	// oldRefreshToken := c.GetCookie(constant.REFRESH_TOKEN_COOKIE)
+	oldRefreshToken := c.GetCookie(constant.REFRESH_TOKEN_COOKIE)
 	jwtProviderId := h.cfg.AuthConfig.JWT.ProviderId
 	var userCred LoginUserAccountType
 
@@ -128,18 +128,20 @@ func (h *Handler) login(c router.Context) error {
 	accessTokenPayload := helper.CreateAccessTokenPayload(userId.Hex(), jwtProviderId, userAccount.Role)
 	refreshTokenPayload := helper.CreateRefreshTokenPayload(userId.Hex(), jwtProviderId, userAccount.Role)
 
-	accessToken, _, err := utils.GenerateRefreshAndAccessToken(accessTokenPayload, refreshTokenPayload, h.cfg)
+	accessToken, newRefreshToken, err := utils.GenerateRefreshAndAccessToken(accessTokenPayload, refreshTokenPayload, h.cfg)
 	if err != nil {
 		return err
 	}
 
-	// h.store.HandleRefreshTokenForLogin(ctx, userId.Hex(), newRefreshToken, oldRefreshToken)
+	h.store.HandleRefreshTokenForLogin(ctx, userId.Hex(), newRefreshToken, oldRefreshToken)
 
-	// if len(oldRefreshToken) != 0 {
-	// helper.ClearCookie(c, constant.REFRESH_TOKEN_COOKIE)
-	// }
+	if len(oldRefreshToken) != 0 {
+		helper.ClearCookie(c, constant.REFRESH_TOKEN_COOKIE)
+	}
 
-	// helper.UpdateCookie(c, constant.REFRESH_TOKEN_COOKIE, newRefreshToken, constant.REFRESH_TOKEN_COOKIE_EXPIRY)
+	helper.UpdateCookie(c, constant.REFRESH_TOKEN_COOKIE, newRefreshToken, constant.REFRESH_TOKEN_COOKIE_EXPIRY)
+
+	fmt.Println("<<<<<<<<<<<<<<<<<<<< hre is the user logged in sucessuflly")
 
 	return utils.SendResponse(c, "User Logged In Succesfully", fiber.Map{"accessToken": accessToken, "role": userAccount.Role, "userProfileId": userAccount.UserProfile}, 200)
 }
