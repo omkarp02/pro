e58cbd610e5566e25584a7e19fd21a379a9080a9
omkar <opwebdev01@gmail.com>
2025-02-26 13:37:59 +0530

    fda

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -74,10 +74,10 @@ func main() {
 
 	validator := validation.NewValidator()
 
-	setUpClothesApp(DB, cfg, validator, api)
+	// setUpClothesApp(DB, cfg, validator, api)
 	setUpAuthApp(DB, cfg, validator, api)
-	setUpMasterApp(DB, cfg, validator, api)
-	setUpAssessment(DB, cfg, validator, api)
+	// setUpMasterApp(DB, cfg, validator, api)
+	// setUpAssessment(DB, cfg, validator, api)
 
 	api.Listen(cfg.HTTPServer.Addr)
 }
