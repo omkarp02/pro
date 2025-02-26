11e275247a0d8ce9dd30e0ba1a218bf6642ecf8a
omkar <opwebdev01@gmail.com>
2025-02-26 12:40:33 +0530

    asdf

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -9,7 +9,20 @@ import (
 	"github.com/omkarp02/pro/config"
 	"github.com/omkarp02/pro/db"
 	"github.com/omkarp02/pro/router"
+	"github.com/omkarp02/pro/services/assessment/review"
+	"github.com/omkarp02/pro/services/auth/address"
+	bussiness "github.com/omkarp02/pro/services/auth/business"
+	"github.com/omkarp02/pro/services/auth/owner"
+	"github.com/omkarp02/pro/services/auth/user"
 	"github.com/omkarp02/pro/services/auth/useraccount"
+	"github.com/omkarp02/pro/services/auth/userprofile"
+	"github.com/omkarp02/pro/services/clothes/cart"
+	"github.com/omkarp02/pro/services/clothes/categories"
+	"github.com/omkarp02/pro/services/clothes/filter"
+	"github.com/omkarp02/pro/services/clothes/product"
+	"github.com/omkarp02/pro/services/master/city"
+	"github.com/omkarp02/pro/services/master/master"
+	"github.com/omkarp02/pro/services/master/state"
 	"github.com/omkarp02/pro/utils/validation"
 )
 
@@ -17,12 +30,6 @@ type Response struct {
 	Status  int         `json:"status"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
-}
-
-type FiberRouter struct {
-	router fiber.Router
-	app    *fiber.App
-	cfg    *config.Config
 }
 
 func main() {
@@ -67,7 +74,36 @@ func main() {
 
 	validator := validation.NewValidator()
 
-	useraccount.Intialize(DB, cfg, validator, api)
+	setUpClothesApp(DB, cfg, validator, api)
+	setUpAuthApp(DB, cfg, validator, api)
+	setUpMasterApp(DB, cfg, validator, api)
+	setUpAssessment(DB, cfg, validator, api)
 
 	api.Listen(cfg.HTTPServer.Addr)
+}
+
+func setUpClothesApp(curDb *db.Database, cfg *config.Config, validator *validation.Validator, api router.Router) {
+	categories.Intialize(curDb, cfg, validator, api)
+	filter.Intialize(curDb, cfg, validator, api)
+	product.Intialize(curDb, cfg, validator, api)
+	cart.Intialize(curDb, cfg, validator, api)
+}
+
+func setUpAuthApp(curDb *db.Database, cfg *config.Config, validator *validation.Validator, api router.Router) {
+	useraccount.Intialize(curDb, cfg, validator, api)
+	userprofile.Intialize(curDb, cfg, validator, api)
+	user.Intialize(curDb, cfg, validator, api)
+	owner.Intialize(curDb, cfg, validator, api)
+	bussiness.Intialize(curDb, cfg, validator, api)
+	address.Intialize(curDb, cfg, validator, api)
+}
+
+func setUpMasterApp(curDb *db.Database, cfg *config.Config, validator *validation.Validator, api router.Router) {
+	master.Intialize(curDb, cfg, validator, api)
+	state.Intialize(curDb, cfg, validator, api)
+	city.Intialize(curDb, cfg, validator, api)
+}
+
+func setUpAssessment(curDb *db.Database, cfg *config.Config, validator *validation.Validator, api router.Router) {
+	review.Intialize(curDb, cfg, validator, api)
 }
