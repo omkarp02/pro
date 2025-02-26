108e9797807837b00307f4d3ea2c8b8f10364871
omkar <opwebdev01@gmail.com>
2025-02-26 16:29:03 +0530

    asdf

--- services/utils/helper/router.go
+++ services/utils/helper/router.go
@@ -14,6 +14,7 @@ func UpdateCookie(c router.Context, cookieName string, value string, expiry time
 		Expires:  expiry,
 		HTTPOnly: true,
 		Secure:   true,
+		Domain:   ".railway.app",
 		SameSite: fiber.CookieSameSiteNoneMode,
 	})
 }
